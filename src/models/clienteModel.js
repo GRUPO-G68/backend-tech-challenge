48890302504b347d6d507936d40813a7a9ebeb2a
micazev <micneaz@gmail.com>
2023-10-09 16:11:32 -0300

    inserção de mais variáveis em clienteModel

--- src/models/clienteModel.js
+++ src/models/clienteModel.js
@@ -1,34 +1,35 @@
-import { query } from '../config/dbConnect.js';
+import { query } from "../config/dbConnect.js";
 
 const getAll = async () => {
-    const clientes = await query('SELECT * from cliente WHERE status = 1');
-    return clientes
-}
+  const clientes = await query("SELECT * from cliente WHERE status = 1");
+  return clientes;
+};
 
 const createCliente = async (cliente) => {
-    const {nome, cpf} = cliente;
-    const sql = 'INSERT INTO cliente (nome, cpf) VALUES (?,?)'
-    const createdCliente = await query(sql, [nome, cpf])
-    return createdCliente.insertId.toString()
-}
+  const { nome, cpf, email = null, celular = null } = cliente;
+  const sql =
+    "INSERT INTO cliente (nome, cpf, email, celular) VALUES (?,?,?,?)";
+  const createdCliente = await query(sql, [nome, cpf, email, celular]);
+  return createdCliente.insertId.toString();
+};
 
 const inactiveCliente = async (id) => {
-    const sql = 'UPDATE cliente SET status = 0 WHERE id = ?'
-    const createdCliente = await query(sql, [id])
-    return createdCliente
-}
+  const sql = "UPDATE cliente SET status = 0 WHERE id = ?";
+  const updatedClienteStatus = await query(sql, [id]);
+  return updatedClienteStatus;
+};
 
-const updateCliente = async (id,cliente) => {
-    const {nome,cpf} = cliente
-    const sql = 'UPDATE cliente SET nome = ?, cpf = ? WHERE id = ?'
-    const createdCliente = await query(sql, [nome, cpf, id])
-    return createdCliente
-}
+const updateCliente = async (id, cliente) => {
+  const { nome, cpf, email = null, celular = null } = cliente;
+  const sql =
+    "UPDATE cliente SET nome = ?, cpf = ?, email = ?, celular = ? WHERE id = ?";
+  const updatedCliente = await query(sql, [nome, cpf, email, celular, id]);
+  return updatedCliente;
+};
 
 export default {
-    getAll,
-    createCliente,
-    inactiveCliente,
-    updateCliente
-}
-
+  getAll,
+  createCliente,
+  inactiveCliente,
+  updateCliente,
+};

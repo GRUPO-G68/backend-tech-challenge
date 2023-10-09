6fbc2e5c68eff1135960bfe964ef5d0882b0d634
Renan Pedrosa <renandspedrosa@gmail.com>
2023-10-08 21:39:16 -0300

    alter table

--- migrations/001-script.sql
+++ migrations/001-script.sql
@@ -1,11 +1,11 @@
-CREATE DATABASE IF NOT EXISTS fiap_db;
-USE fiap_db;
-
-CREATE TABLE IF NOT EXISTS clientes (
-    ID INT (11) AUTO_INCREMENT,
-    nome VARCHAR(255),
-    cpf CHAR(11),
-    PRIMARY KEY (ID)
+CREATE TABLE `cliente` (
+	`ID` INT(11) NOT NULL AUTO_INCREMENT,
+	`nome` VARCHAR(255) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
+	`cpf` CHAR(11) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
+	`created_at` TIMESTAMP NOT NULL DEFAULT current_timestamp(),
+	`updated_at` TIMESTAMP NULL DEFAULT NULL,
+	`status` CHAR(1) NULL DEFAULT '1' COLLATE 'utf8mb4_general_ci',
+	PRIMARY KEY (`ID`) USING BTREE
 );
 
 INSERT INTO `fiap_db`.`clientes` (`nome`, `cpf`) VALUES ('JOSE', '65332844018');

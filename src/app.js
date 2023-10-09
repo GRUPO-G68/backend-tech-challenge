988044174eb6f52441e650604760d1d064be63e4
Renan Pedrosa <renandspedrosa@gmail.com>
2023-10-09 00:09:52 -0300

    removendo

--- src/app.js
+++ src/app.js
@@ -4,10 +4,6 @@ import routes from "./routes/index.js"
 const app = express()
 const port = process.env.PORT
 
-//pega o que esta chegando via POST ou PUT e transforma um objeto para pode manipular
-app.use(express.json())
-
 routes(app)
-
 export default app
 

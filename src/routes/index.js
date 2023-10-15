67f1ce4eb0512b045b456706e376947005a32955
Renan Pedrosa <renandspedrosa@gmail.com>
2023-10-15 09:25:44 -0300

    apagando comentario

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,16 +11,6 @@ const routes = (app) => {
   app.use(express.json(), clientes);
   app.use(express.json(), funcionarios);
   app.use(express.json(), doc);
-
-  /*docs*/
-  // app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-  // app.use('/swagger', (_req,res) => {
-  //   return res.sendFile(process.cwd()+'/swagger.json')
-  // });
-  // app.use('/documentacao', (_req,res) => {
-  //   return res.sendFile(process.cwd()+'/index.html')
-  // });
-
 };
 
 export default routes;

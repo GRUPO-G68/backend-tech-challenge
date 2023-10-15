e0365806fc82540e0158413a34f9919f8d3e3385
Renan Pedrosa <renandspedrosa@gmail.com>
2023-10-15 09:24:54 -0300

    espaço

--- src/routes/docRoutes.js
+++ src/routes/docRoutes.js
@@ -4,7 +4,6 @@ import swaggerUi from "swagger-ui-express";
 import swaggerDocument from '../../swagger.json' assert { type: 'json' };
 
 
-
 router.get("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 router.get("/swagger", (_req,res) => {
       return res.sendFile(process.cwd()+'/swagger.json')

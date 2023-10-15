e10a12dbe1380e4c74b0a6aa7f70c0ef69731db2
Renan Pedrosa <renandspedrosa@gmail.com>
2023-10-15 09:25:07 -0300

    esp

--- src/routes/docRoutes.js
+++ src/routes/docRoutes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 import swaggerUi from "swagger-ui-express";
 import swaggerDocument from '../../swagger.json' assert { type: 'json' };
 
-
 router.get("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 router.get("/swagger", (_req,res) => {
       return res.sendFile(process.cwd()+'/swagger.json')

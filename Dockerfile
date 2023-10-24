5c653aac8835d1f0a2d378715b7b418d439f5b9b
Renan Pedrosa <renandspedrosa@gmail.com>
2023-10-23 21:45:45 -0300

    colocando swagger

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,7 @@ WORKDIR /home/node/app
 COPY package.json .
 COPY tsconfig.json .
 COPY src/ .
+COPY swagger.json .
+COPY index.html .
 RUN npm install
 CMD ["npm", "run", "dev"]
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import { config } from "dotenv";
 import Database from "./infrastructure/database";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "../swagger.json";
 
 config();
 
@@ -10,6 +12,17 @@ const port = process.env.PORT || 9001;
 
 const db = new Database();
 
+/*docs*/
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/swagger", (_req, res) => {
+  return res.sendFile(process.cwd() + "/swagger.json");
+});
+app.use("/documentacao", (_req, res) => {
+  return res.sendFile(process.cwd() + "/index.html");
+});
+
+app.listen(port, () => console.log(`listening on port ${port}`));
+
 app.get("/", async (req, res) => {
   try {
     const results = await db.query("SELECT * FROM Cliente", []);
@@ -18,5 +31,3 @@ app.get("/", async (req, res) => {
     res.status(500).json({ error: "Internal server error" });
   }
 });
-
-app.listen(port, () => console.log(`listening on port ${port}`));

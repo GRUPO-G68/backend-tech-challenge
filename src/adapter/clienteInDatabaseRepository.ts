1d9815804ac441ad95f293cbdda045546f089cf5
Renan Pedrosa <renandspedrosa@gmail.com>
2023-10-25 19:24:46 -0300

    melhorando infra db

--- src/adapter/clienteInDatabaseRepository.ts
+++ src/adapter/clienteInDatabaseRepository.ts
@@ -3,14 +3,17 @@ import { IClienteRepository } from "../applications/ports/clienteRepository";
 import Database from "../infra/database";
 
 export class clienteInDatabaseRepository implements IClienteRepository {
+  db: Database;
+  constructor() {
+    this.db = new Database();
+  }
+
   async findAll(): Promise<Cliente | null> {
-    const db = new Database();
-    return await db.query(`SELECT * FROM Cliente`, []);
+    return await this.db.query(`SELECT * FROM Cliente`);
   }
 
   async findByCpf(cpf: string): Promise<Cliente | null> {
-    const db = new Database();
-    return await db.query(`SELECT * FROM Cliente WHERE cpf = ${cpf}`, []);
+    return await this.db.query(`SELECT * FROM Cliente WHERE cpf = ${cpf}`);
   }
 
   async save(): Promise<void> {}

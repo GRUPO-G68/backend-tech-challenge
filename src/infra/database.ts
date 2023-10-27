a9f10c00c150b77829cdb0d38337b7a43f0f7f94
Estêvão Santiago Vieira de Moraes Costa <esantiagovieira@gmail.com>
2023-10-26 21:10:54 -0300

    corrige alteração feita no database.ts para teste local

--- src/infra/database.ts
+++ src/infra/database.ts
@@ -8,7 +8,7 @@ class Database {
 
   constructor() {
     this.pool = mariadb.createPool({
-      // host: "localhost:3306",
+      host: process.env.MYSQL_HOST,
       user: process.env.MYSQL_USER,
       password: process.env.MYSQL_PASSWORD,
       database: process.env.MYSQL_DB_NAME,

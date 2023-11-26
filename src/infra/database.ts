40820f02a37a1d0e0dd0e1b76f2ceb43f4a61347
Renan Pedrosa <renandspedrosa@gmail.com>
2023-11-26 10:34:42 -0300

    mais ajustes

--- src/infra/database.ts
+++ src/infra/database.ts
@@ -9,6 +9,7 @@ class Database {
   constructor() {
     this.pool = mariadb.createPool({
       host: process.env.MYSQL_HOST,
+      port: parseInt(process.env.MYSQL_PORT || "3306", 10),
       user: process.env.MYSQL_USER,
       password: process.env.MYSQL_PASSWORD,
       database: process.env.MYSQL_DB_NAME,

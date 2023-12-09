7e0d9b596edd97cb1b9cb93f4fc35c072396cb5c
Alan Bonachella <alanbonachella@gmail.com>
2023-12-09 16:09:19 -0300

    feat: add nest swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { NestExpressApplication } from "@nestjs/platform-express";
+import { SwaggerModule, DocumentBuilder, OpenAPIObject } from "@nestjs/swagger";
 
+function buildApiDocs(app: NestExpressApplication): void {
+  const config: Omit<OpenAPIObject, "paths"> = new DocumentBuilder()
+    .setTitle("FIAP - Software Architecture - Tech Challenge")
+    .setDescription(
+      "Tech challenge for postgraduate studies in software architecture",
+    )
+    .setVersion("1.0")
+    .addTag("FIAP - Pós Software Architecture")
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("docs", app, document);
+}
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app: NestExpressApplication = await NestFactory.create(AppModule);
+  buildApiDocs(app);
   await app.listen(3000);
 }
 bootstrap();

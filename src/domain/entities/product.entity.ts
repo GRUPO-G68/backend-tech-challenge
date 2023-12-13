6b69b42b5f5fdec928a0c4ddaefa7210d3da2745
Alan Bonachella <alanbonachella@gmail.com>
2023-12-13 19:42:36 -0300

    refactor: adjust product entity and add decorators db model

--- src/domain/entities/product.entity.ts
+++ src/domain/entities/product.entity.ts
@@ -1,3 +1,5 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+
 export interface IProduct {
   id: string;
   name: string;
@@ -5,18 +7,27 @@ export interface IProduct {
   category: string;
   price: number;
   status: string;
-  createdAt: string;
-  updatedAt: string;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
 // @todo adicionar anotaçoes para o banco de dados
+@Entity()
 export class Product implements IProduct {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
+  @Column({ type: 'text', nullable: false })
   category: string;
+  @Column({ type: 'text', nullable: true })
   description: string;
+  @Column({ type: 'text', nullable: false })
   name: string;
+  @Column({ type: 'integer', nullable: false })
   price: number;
+  @Column({ type: 'text', nullable: false })
   status: string;
-  createdAt: string;
-  updatedAt: string;
+  @CreateDateColumn({ type: 'datetime', generated: true, nullable: false })
+  createdAt: Date;
+  @UpdateDateColumn({ type: 'datetime', generated: true, nullable: false })
+  updatedAt: Date;
 }
